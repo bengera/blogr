4a107f29d34e3d16c6de6c1616374ce22d1bdc63
unknown <bengera89@gmail.com>
2021-11-09 10:28:17 +0700

    basic-nav-js

--- js/index.js
+++ js/index.js
@@ -2,13 +2,36 @@
 
 const hamburgerNav = document.getElementById('btn-hamburger')
 const mobileNav = document.querySelector('.mobile-navigation')
+const hamburgerImage = document.querySelector('.header__hamburger')
+
+const product = document.getElementById('product')
+const navigationInner = document.querySelector('.mobile-navigation__inner')
+
+const arrow = document.querySelector('.mobile-navigation__arrow')
+
 
 hamburgerNav.addEventListener('click', () => {
     if (mobileNav.style.display === "none") {
         mobileNav.style.display="block";
         
+        
     } else {
         mobileNav.style.display = "none";
+       
+    }
+    
+});
+
+product.addEventListener('click', () => {
+    if (navigationInner.style.display === "none") {
+        navigationInner.style.display="block";
+        arrow.style.transform="rotate(180deg)";
+        
+        
+    } else {
+        navigationInner.style.display = "none";
+        arrow.style.transform="rotate(0deg)";
+
        
     }
     
